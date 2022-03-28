b063b0f68c36e44d13be403ea7fb79249cc87f79
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-28 10:41:24 +0600

    install tailwined

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>ctailwind-pricing-club</h1>
+      <h1 className='text-3xl font-bold underline text-yellow-300'>ctailwind-pricing-club</h1>
     </div>
   );
 }
